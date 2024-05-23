67675ab0a5197c2ca46a5c760715a12c9dfbe1da
yosser16 <yossermerida@outlook.fr>
2024-05-23 11:54:15 +0100

    first commit

---
+++ check.js
@@ -0,0 +1,13 @@
+function insertionSort(array) {
+    for(let i = 1; i < array.length; i++) {
+        let key = array[i];
+        let j = i - 1;
+        while (j >= 0 && array[j] > key) {
+            array[j + 1] = array[j];
+            j = j - 1;
+        }
+        array[j + 1] = key;
+    }
+    return array;
+} 
+console.log(insertionSort([500,200,400,20,7,6000]));
